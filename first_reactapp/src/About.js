1cc3ab694190c5c3d69bfb6b0adfdadd73f8de3e
ClafgJayKay <phangjunkai@gmail.com>
2022-09-26 19:11:02 +0800

    added conditional classes for colors based on age
    added conditional classes for colors based on age

--- first_reactapp/src/About.js
+++ first_reactapp/src/About.js
@@ -1,11 +1,32 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import {useState} from "react"
 
 function About(){
+    let peopleList = [
+        {"name":"john",'age':55},
+        {"name":"jane",'age':55},
+        {"name":"aaron",'age':20},
+        {"name":"may",'age':20},
+    ];
+    let name = "this is a name";
+    const[headingClassName,setHeadingClassName] = useState('heading_1');
+
     return(
         <div>
             <Header/>
             <h1>This is the header of the About Page</h1>
+            <h2 className={headingClassName}>{name}</h2>
+            <button onClick={()=>setHeadingClassName('heading_1')}>Heading Class 1</button>
+            <button onClick={()=>setHeadingClassName('heading_2')}>Heading Class 2</button>
+            <button onClick={()=>setHeadingClassName('heading_3')}>Heading Class 3</button>
+            <ul>
+                {
+                   peopleList.map((peopleObj,idx)=>{
+                    return <li key={idx} className={peopleObj.age>50?'heading_1':"heading_2"}>{peopleObj.name} -- {peopleObj.age}</li>
+                   }) 
+                }
+            </ul>
             <Footer/>
         </div>
     )
