7eec7bcfc5e244a274f0029c86813a63c177cdb3
ClafgJayKay <phangjunkai@gmail.com>
2022-10-11 21:51:16 +0800

    Updated with spring boot integration (oct11)
    Updated with spring boot integration (oct11)

--- first_reactapp/src/Contacts.js
+++ first_reactapp/src/Contacts.js
@@ -1,11 +1,60 @@
+import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function Contacts(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [output, setOutput] = useState("");
+
+    const postBtn = () => {
+        let exampleObj = {"username": username, "password": password};
+
+            fetch("http://localhost:8080/login",
+            {
+                method:"POST",
+                body:JSON.stringify(exampleObj),
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(res => res.json())
+            .then(res2 => {console.log(res2); setOutput(res2)})
+            .catch(err => console.log(err))   
+    }
+
     return(
         <div>
             <Header menuname='contacts'/>
-            <h1>This is the header of the Contacts page</h1>
+
+            <div className="container-fluid py-5">
+                <div className="container py-5">
+                   
+                    <div className="row justify-content-center">
+                        <div className="col-lg-9">
+                            <div className="contact-form bg-light rounded p-5">
+                                <div id="success"></div>
+                                <form name="sentMessage" id="contactForm" noValidate="novalidate">
+                                    <div className="form-row">
+                                        <div className="col-sm-6 control-group">
+                                            <input onChange={e => setUsername(e.target.value)} type="text" className="form-control p-4" id="name" placeholder="Enter username" required="required" data-validation-required-message="Please enter a name" />
+                                            <p className="help-block text-danger"></p>
+                                        </div>
+                                        <div className="col-sm-6 control-group">
+                                            <input onChange={e => setPassword(e.target.value)} type="text" className="form-control p-4" placeholder="Enter password" required="required" data-validation-required-message="Please enter a password" />
+                                            <p className="help-block text-danger"></p>
+                                        </div>
+                                    </div>
+                                </form>
+                                <div>
+                                    <button onClick={postBtn} className="btn btn-primary btn-block py-3 px-5" type="submit" id="sendMessageButton">Login</button>
+                                </div>
+                                <span>{JSON.stringify(output)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
