6cbf483bfe492b9e3203c2737090cc5f5a19fa6c
ClafgJayKay <phangjunkai@gmail.com>
2022-10-12 20:41:44 +0800

    12 oct new proj
    12 oct new proj

---
+++ 12oct2022/src/test/java/com/oct2022/oct2022/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oct2022.oct2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
