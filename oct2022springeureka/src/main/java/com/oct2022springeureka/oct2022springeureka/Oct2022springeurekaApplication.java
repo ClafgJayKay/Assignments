075a77908bb72fa759d66d39e8098a6720ae3596
ClafgJayKay <phangjunkai@gmail.com>
2022-10-17 18:17:31 +0800

    created new proj for eureka server
    created new proj for eureka server

--- oct2022springeureka/src/main/java/com/oct2022springeureka/oct2022springeureka/Oct2022springeurekaApplication.java
+++ oct2022springeureka/src/main/java/com/oct2022springeureka/oct2022springeureka/Oct2022springeurekaApplication.java
@@ -2,8 +2,10 @@ package com.oct2022springeureka.oct2022springeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Oct2022springeurekaApplication {
 
 	public static void main(String[] args) {
