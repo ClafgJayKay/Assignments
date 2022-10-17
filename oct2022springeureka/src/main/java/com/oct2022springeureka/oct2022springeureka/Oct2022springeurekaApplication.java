d0bd61dc2a80ee7d31704d40493c1dd38ac4437d
ClafgJayKay <phangjunkai@gmail.com>
2022-10-17 18:10:50 +0800

    spring eureka server

---
+++ oct2022springeureka/src/main/java/com/oct2022springeureka/oct2022springeureka/Oct2022springeurekaApplication.java
@@ -0,0 +1,13 @@
+package com.oct2022springeureka.oct2022springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oct2022springeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oct2022springeurekaApplication.class, args);
+	}
+
+}
---
+++ oct2022springeureka/src/test/java/com/oct2022springeureka/oct2022springeureka/Oct2022springeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oct2022springeureka.oct2022springeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oct2022springeurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
