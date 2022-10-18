e3f941d3b8f30743863141a2228eab5eab2969a1
ClafgJayKay <phangjunkai@gmail.com>
2022-10-18 21:20:45 +0800

    updated service 1
    updated service 1

--- spring_microservices/service1/src/main/java/com/springcloudproj/service1/Service1Application.java
+++ spring_microservices/service1/src/main/java/com/springcloudproj/service1/Service1Application.java
@@ -2,8 +2,12 @@ package com.springcloudproj.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Service1Application {
 
 	public static void main(String[] args) {
