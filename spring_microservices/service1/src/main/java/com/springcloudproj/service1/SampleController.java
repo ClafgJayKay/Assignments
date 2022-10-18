45a19126a3455bc8526083cff7d1b05f4ec27410
ClafgJayKay <phangjunkai@gmail.com>
2022-10-18 22:10:41 +0800

    created gateway
    created gateway

---
+++ spring_microservices/service1/src/main/java/com/springcloudproj/service1/SampleController.java
@@ -0,0 +1,24 @@
+package com.springcloudproj.service1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("service1")
+@RestController
+public class SampleController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("sampleget")
+    public String callAPI(){
+        String response = restTemplate.getForObject("http://localhost:8082/sampleapi", String.class);
+
+        System.out.println("another service response is: "+response);
+        return response;
+    }
+}
