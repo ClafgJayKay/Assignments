e21ca4a959ab692651a003256813922f08a9be0e
ClafgJayKay <phangjunkai@gmail.com>
2022-10-18 22:27:53 +0800

    added sample circuit breaker
    added sample circuit breaker

--- spring_microservices/service1/src/main/java/com/springcloudproj/service1/SampleController.java
+++ spring_microservices/service1/src/main/java/com/springcloudproj/service1/SampleController.java
@@ -2,6 +2,8 @@ package com.springcloudproj.service1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,8 @@ public class SampleController {
 
     @Autowired
     RestTemplate restTemplate;
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
 
     @GetMapping("sampleget")
     public String callAPI(){
@@ -20,5 +24,18 @@ public class SampleController {
 
         System.out.println("another service response is: "+response);
         return response;
+    }
+
+    private String callWhenError(){
+        return "error when calling the api";
+    }
+
+    @GetMapping("samplegettest")
+    public String GetList(){
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+        String url = "https://jsonplaceholder.typicode.com/albums";
+
+        return circuitBreaker.run(()->restTemplate.getForObject(url,String.class),
+            throwable -> callWhenError());
     }
 }
